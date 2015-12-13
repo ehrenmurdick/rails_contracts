1edda96cf1f1ea9c0ef1f7a9f3d104b626eb5785
Ehren Murdick <pair+emurdick@pivotallabs.com>
2015-12-12 23:31:11 -0800

    edit show update links

--- spec/views/topics/show.html.haml_spec.rb
+++ spec/views/topics/show.html.haml_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 describe 'topics/show.html.haml' do
-  let(:topic) { double(:topic) }
+  Contract.create('Topic#name ""')
+  Contract.create('Topic#to_param "1"')
+  let(:topic) { double(:topic, to_param: '1', name: 'A name') }
+
   before do
     Contract.create('TopicsController#show assigns @topic')
     assign(:topic, topic)
-
-    Contract.create('Topic#name ""')
-    expect(topic).to receive(:name).and_return('A name')
     render
   end
 
   it { expect(rendered).to include('A name') }
+
+  Contract.create('TopicsController#edit')
+  it { expect(css_select('a[href="/topics/1/edit"]')).to_not be_empty }
+
+  Contract.create('TopicsController#destroy')
+  it { expect(css_select('a[data-method="delete"][href="/topics/1"]')).to_not be_empty }
+
+  Contract.create('TopicsController#index')
+  it { expect(css_select('a[href="/topics"]')).to_not be_empty }
 end
