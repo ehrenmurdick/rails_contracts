10381288740b2547ba00e8aaddaa94b6c56e81e5
Ehren Murdick <pair+emurdick@pivotallabs.com>
2015-12-12 23:15:50 -0800

    edit form

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -37,4 +37,24 @@ describe Topic do
     Contract.fulfill('Topic#to_param "1"')
     it { expect(first.to_param).to eql('309456473') }
   end
+
+  describe '#model_name' do
+    Contract.fulfill('Topic#model_name Topic.model_name')
+    it { expect(first.model_name.object_id).to eql(Topic.model_name.object_id) }
+  end
+
+  describe '#to_key' do
+    Contract.fulfill('Topic#to_key [1]')
+    it { expect(first.to_key).to eql([first.id]) }
+  end
+
+  describe '#to_model' do
+    Contract.fulfill('Topic#to_model topic')
+    it { expect(first.to_model).to eql(first) }
+  end
+
+  describe '#persisted?' do
+    Contract.fulfill('Topic#persisted? true')
+    it { expect(first.persisted?).to eql(true) }
+  end
 end
--- spec/views/topics/edit.html.haml_spec.rb
+++ spec/views/topics/edit.html.haml_spec.rb
@@ -8,6 +8,26 @@ describe 'topics/edit.html.haml' do
 
     Contract.create('Topic#name ""')
     expect(topic).to receive(:name).and_return('A name')
+
+    Contract.create('Topic#model_name Topic.model_name')
+    expect(topic).to receive(:model_name).
+      at_least(:once).
+      and_return(Topic.model_name)
+
+    Contract.create('Topic#to_key [1]')
+    expect(topic).to receive(:to_key).
+      and_return([1])
+
+    Contract.create('Topic#to_model topic')
+    expect(topic).to receive(:to_model).
+      at_least(:once).
+      and_return(topic)
+
+    Contract.create('Topic#persisted? true')
+    expect(topic).to receive(:persisted?).
+      at_least(:once).
+      and_return(true)
+
     render
   end
 
